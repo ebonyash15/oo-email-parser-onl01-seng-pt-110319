1db90818dcae4fbf156871f7e605437cb719736d
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-04 06:20:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  def initalize(emails)
+  def initialize(emails)
     @emails = emails
   end
   def parse
