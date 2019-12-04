d9cd1c31f434849d8078c4209e48ffe8168bd0a8
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-04 07:12:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,7 @@ class EmailAddressParser
   p  all_commas=@emails.sub!(" ",",")
   p  seperated=all_commas.split(",")
     seperated.each do |email|
-      if !into.include?(email)&&email!=","&&email!=""
+      if !into.include?(email)&&email!=""
         into<<email
       end
     end
